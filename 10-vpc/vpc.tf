cfdfcff7586f023f42273e7df254ddc18150ff5d
vrapracash <veeraprakash.bg@gmail.com>
2025-02-14 22:21:45 +0530

    script-update

---
+++ 10-vpc/vpc.tf
@@ -0,0 +1,11 @@
+module "vpc" {
+    source                  = "git::https://github.com/vrapracash/terraform-aws-vpc.git?ref=main"
+    vpc_cidr                = var.vpc_cidr
+    project_name            = var.project_name
+    environment             = var.environment
+    common_tags             = var.common_tags
+    public_subnet_cidrs     = var.public_subnet_cidrs
+    private_subnet_cidrs    = var.private_subnet_cidrs
+    database_subnet_cidrs   = var.database_subnet_cidrs
+    is_peering_required     = var.is_peering_required
+}
