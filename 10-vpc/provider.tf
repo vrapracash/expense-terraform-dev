111c0b36b63fc13a4793a89c829b455e7de90a25
vrapracash <veeraprakash.bg@gmail.com>
2025-02-16 13:08:52 +0530

    bastion

--- 10-vpc/provider.tf
+++ 10-vpc/provider.tf
@@ -1,16 +1,16 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.66.0"
     }
   }
 
   backend "s3" {
-    bucket          = "vrapracash-1"
-    key             = "workspaces"
-    region          = "us-east-1"
-    dynamodb_table  = "vrapracash-1-table" 
+    bucket         = "vrapracash-1"
+    key            = "workspaces"
+    region         = "us-east-1"
+    dynamodb_table = "vrapracash-1-table"
   }
 }
 
--- 10-vpc/variables.tf
+++ 10-vpc/variables.tf
@@ -3,29 +3,29 @@ variable "vpc_cidr" {
 }
 
 variable "project_name" {
-    default = "expense"
+  default = "expense"
 }
 
 variable "environment" {
-    default = "dev"
+  default = "dev"
 }
 
 variable "common_tags" {
-    default = {}
+  default = {}
 }
 
 variable "public_subnet_cidrs" {
-    default = ["10.0.1.0/24", "10.0.2.0/24"]
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnet_cidrs" {
-    default = ["10.0.11.0/24", "10.0.12.0/24"]
+  default = ["10.0.11.0/24", "10.0.12.0/24"]
 }
 
 variable "database_subnet_cidrs" {
-    default = ["10.0.21.0/24", "10.0.22.0/24"]
+  default = ["10.0.21.0/24", "10.0.22.0/24"]
 }
 
 variable "is_peering_required" {
-    default = true
+  default = true
 }
